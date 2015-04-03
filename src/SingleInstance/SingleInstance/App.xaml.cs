ba57ef817abc40cdc9d3ded7925821750f7a9a2d
Tomáš Hübelbauer <tomas@hubelbauer.net>
2015-04-03 23:09:14 +0200

    Add logging statements to connection workflow

--- src/SingleInstance/SingleInstance/App.xaml.cs
+++ src/SingleInstance/SingleInstance/App.xaml.cs
@@ -16,8 +16,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using log4net;
-using log4net.Appender;
-using log4net.Config;
 
 namespace SingleInstance
 {
@@ -38,6 +36,16 @@ namespace SingleInstance
     #region Fields
 
     /// <summary>
+    /// The log.
+    /// </summary>
+    private static readonly ILog Log = LogManager.GetLogger(typeof(App));
+
+    /// <summary>
+    /// The process ID.
+    /// </summary>
+    private static readonly Int32 Pid = Process.GetCurrentProcess().Id;
+
+    /// <summary>
     /// The mutex.
     /// </summary>
     private static Mutex mutex;
@@ -46,11 +54,6 @@ namespace SingleInstance
     /// The cancellation token source.
     /// </summary>
     private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-
-    /// <summary>
-    /// The log.
-    /// </summary>
-    private readonly ILog log = LogManager.GetLogger(typeof(App));
 
     #endregion
 
@@ -64,15 +67,11 @@ namespace SingleInstance
     /// </param>
     protected override async void OnStartup(StartupEventArgs e)
     {
-      AppDomain.CurrentDomain.UnhandledException += (sender, e2) =>
-      {
-        log.Error("Unhandled eception.", (Exception)e2.ExceptionObject);
-        var halt = true;
-      };
+      AppDomain.CurrentDomain.UnhandledException += (sender, e2) => Log.Error(String.Format("{0}: Unhandled eception.", Pid), (Exception)e2.ExceptionObject);
 
       if (TryAcquireMutex())
       {
-        this.log.InfoFormat("Process #{0} is the server.", Process.GetCurrentProcess().Id);
+        Log.InfoFormat("{0} is the server.", Pid);
 
         // Use the constructor instead of the Run static method or variable assignment to avoid ReSharper await suggestion. In this case the task is not to be awaited as 
         new Task(async () =>
@@ -80,10 +79,13 @@ namespace SingleInstance
           // Start the named pipe server stream thread in parallel with the main thread, until the application is shutting down at which point the mutex is released.
           await MonitorInstances(this.cancellationTokenSource.Token).ContinueWith(t => ReleaseMutex());
         }).Start();
+
+        // Simulate a client immediately connecting.
+        this.OnStartup(e);
       }
       else
       {
-        this.log.InfoFormat("Process #{0} is the client.", Process.GetCurrentProcess().Id);
+        Log.InfoFormat("{0} is the client.", Pid);
 
         // Hand the command line arguments over to the sole instance for them to be processed and exit self.
         await DelegateCommandLineArguments(e.Args);
@@ -117,6 +119,7 @@ namespace SingleInstance
     {
       var createdNew = default(Boolean);
       mutex = new Mutex(true, Name, out createdNew);
+      Log.InfoFormat("{0}: mutex acquisition: {1}", Pid, createdNew);
       return createdNew;
     }
 
@@ -141,9 +144,13 @@ namespace SingleInstance
     /// </returns>
     private static async Task MonitorInstances(CancellationToken cancellationToken)
     {
+      Log.InfoFormat("{0}: Instantiating the named pipe server stream…", Pid);
+
       // Create a named pipe server stream that listens to the named pipe clients handing over the command line arguments.
       using (var namedPipeServerStream = new NamedPipeServerStream(Name, PipeDirection.In, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous))
       {
+        Log.InfoFormat("{0}: Instantiated the named pipe server stream.", Pid);
+
         // Continue monitors the late instances until the sole instance is shutting down.
         while (!cancellationToken.IsCancellationRequested)
         {
@@ -166,11 +173,18 @@ namespace SingleInstance
     /// </returns>
     private static async Task DelegateCommandLineArguments(IEnumerable<String> commandLineArguments)
     {
+      Log.InfoFormat("{0}: Instantiating the named pipe client stream…", Pid);
+
       // Create a named pipe client stream that connects to the named pipe server in the sole instance.
       using (var namedPipeClientStream = new NamedPipeClientStream(".", Name, PipeDirection.Out))
       {
+        Log.InfoFormat("{0}: Instantiated the named pipe client stream.", Pid);
+        Log.InfoFormat("{0}: Connecting to the named pipe server stream.", Pid);
+
         // Connect to the named pipe server instance.
         namedPipeClientStream.Connect();
+
+        Log.InfoFormat("{0}: Connected to the named pipe server stream.", Pid);
 
         // Write the command line arguments to the client outward stream a line each.
         using (var streamWriter = new StreamWriter(namedPipeClientStream))
